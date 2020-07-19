9bdf437a3dd0f5c194672b0c569c104d6245e6d4
coolusername244 <leesheppard2404@gmail.com>
2020-07-19 14:12:03 +0000

    Added more functionality to testing - making sure that testing results has been reset before the next test

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,7 +1,11 @@
-function addition (num1, num2) {
-    if(typeof(num1) == "number" && typeof(num2) == "number") {      // This function is checking to see if both parameters are numbers, if they are, then its job is to add them together
-    return num1 + num2;
-    } else {
-        return "Error!";            // if one of the parameters are not a number, it will return an error - DEFENSIVE PROGRAMMING
-    }           
+Calculator = function() {
+    this.value = 0;
+}
+
+Calculator.prototype.add = function(number) {
+    if (typeof(number) == "number") {               // The calculator has changed to display a change in requirements
+        this.value += number;                       // and show that testing will need to be rewritten to make sure 
+    } else {                                        // that the new code passes
+        alert("Error!");
+    }
 }
--- specs/calcSpec.js
+++ specs/calcSpec.js
@@ -1,13 +1,24 @@
-describe("calculator", function(){              
-    describe("Addition tests", function(){      
-        it("should return 42", function(){      
-            expect(addition(20,22)).toBe(42);   
+describe("calculator", function(){       
+    
+    beforeEach(function(){                          // The 'beforeEach' function makes sure that the calculator has
+        calc = new Calculator;                      // reset to 0 before starting the next test.
+    });                                             // Without the 'beforeEach' function, the tests will fail and give
+                                                    // a result of 68 because that is the total of all numbers below.
+    describe("Addition tests", function(){          // The calculator will keep on adding until it is told to reset.
+        it("should return 42", function(){    
+            calc.add(20);
+            calc.add(22);  
+            expect(calc.value).toBe(42);   
         });
         it("should return 26", function() {
-            expect(addition(7, 19)).toBe(26);
+            calc.add(7);
+            calc.add(19);
+            expect(calc.value).toBe(26);
         });
-        it("should return an error if we don't supply two numbers", function() { // To make sure that only numbers are entered, we test that if 2 strings are put in as perameters, it returns the error - ref calc.js
-            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        it("should return an error if we don't supply two numbers", function() {
+            spyOn(window, "alert");
+            calc.add("Hitchhikers");
+            expect(window.alert).toHaveBeenCalledWith("Error!");
         });
     });
 });
