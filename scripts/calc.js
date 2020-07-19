4ab80402799b056fbf069b651413a622d90e87f4
coolusername244 <leesheppard2404@gmail.com>
2020-07-19 12:48:02 +0000

    added a test for strings being inputted to the caculator

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,4 +1,7 @@
-// the calculator is now more advanced, it will take 2 arguments (num1, num2) and return the addition of the two inputs.
 function addition (num1, num2) {
+    if(typeof(num1) == "number" && typeof(num2) == "number") {      // This function is checking to see if both parameters are numbers, if they are, then its job is to add them together
     return num1 + num2;
+    } else {
+        return "Error!";            // if one of the parameters are not a number, it will return an error - DEFENSIVE PROGRAMMING
+    }           
 }
--- specs/calcSpec.js
+++ specs/calcSpec.js
@@ -1,15 +1,13 @@
-// we now have 3 tests that will pass within Jasmine
-
-describe("calculator", function(){              // this is the name of what we are testing and will be showed in Jasmine
-    describe("Addition tests", function(){      // specifically what we are testing
-        it("should return 42", function(){      // what we expect to see
-            expect(addition(20,22)).toBe(42);   // addition function in use with expected outcome - will return true/false
+describe("calculator", function(){              
+    describe("Addition tests", function(){      
+        it("should return 42", function(){      
+            expect(addition(20,22)).toBe(42);   
         });
         it("should return 26", function() {
             expect(addition(7, 19)).toBe(26);
-        })
-        it("should return 100", function() {
-            expect(addition(90, 10)).toBe(100);
-        })
+        });
+        it("should return an error if we don't supply two numbers", function() { // To make sure that only numbers are entered, we test that if 2 strings are put in as perameters, it returns the error - ref calc.js
+            expect(addition("Hitchhikers", "Guide")).toBe("Error!");
+        });
     });
 });
